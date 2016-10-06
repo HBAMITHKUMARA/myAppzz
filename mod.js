a4d82919b76fce1f61d3760ab9bb3b00442491d5
AMITHKUMARA H B <amithkumar.hb94@gmail.com>
2016-10-06 18:37:39 +0530

    On 6th Oct 2016

--- mod.js
+++ mod.js
@@ -1,13 +1,41 @@
 var app = angular.module("myApp",["ngRoute"]);
 app.config(function($routeProvider) {
-            $routeProvider
+        $routeProvider
+            .when("/basic", {
+                templateUrl : "basic.html",
+                controller : "myController"
+            })
             .when("/edu", {
                 templateUrl : "edu.html"
             })
-            .when("/page3", {
-                templateUrl : "edu.html"
+            .when("/exp", {
+                templateUrl : "exp.html"
+            })
+            .when("/skills", {
+                templateUrl : "skills.html"
+            })
+            .when("/achieve", {
+                templateUrl : "achieve.html"
+            })
+            .when("/personal", {
+                templateUrl : "personal.html"
+            })
+            .when("/network", {
+                templateUrl : "network.html"
+            })
+            .when("/feedback", {
+                templateUrl : "feedback.html"
+            })
+            .when("/aboutpage", {
+                templateUrl : "aboutpage.html"
+            })
+            .when("/certification", {
+                templateUrl : "certification.html"
+            })
+            .when("/test2", {
+                templateUrl : "test2.html"
             })
             .otherwise({
-                redirectTo: "/"
-            });
+                redirectTo: "/basic"
         });
+});
